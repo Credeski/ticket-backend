2ef2cb2b62fc8cfba9687ddd15471b0eb311d7c9
demonflowne <ibuemmanuel60@gmail.com>
2024-09-12 20:48:39 +0100

    evrything works

--- src/middlewares/isUserAuthorized.ts
+++ src/middlewares/isUserAuthorized.ts
@@ -2,7 +2,7 @@ import ErrorHandler from "$/utils/errorHandler";
 import { type Request, type Response, type NextFunction } from "express";
 
 declare global {
-    
+    // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Express {
         interface Request {
             role: string;
